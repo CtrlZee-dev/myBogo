7b5169e52546bdd7d20b9ca0cc0b64f71debda10
CtrlZee-dev <142331580+CtrlZee-dev@users.noreply.github.com>
2024-06-26 20:01:51 +0200

    Update logOut.php

--- logOut.php
+++ logOut.php
@@ -7,7 +7,7 @@ session_unset();
 session_destroy();
 
 // Redirect to index.php
-header("Location: index.php");
+header("Location: index.html");
 
 // Execute JavaScript to clear localStorage
 echo "<script>

6cbed6bf1a05d4e8de3445205f7ea153c38a66ec
CtrlZee-dev <142331580+CtrlZee-dev@users.noreply.github.com>
2024-08-14 16:41:22 +0200

    Delete project directory

--- project/checkLogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Check if user is logged in
-if (isset($_SESSION['user_id'])) {
-    // User is logged in
-    echo "logged_in";
-} else {
-    // User is not logged in
-    echo "not_logged_in";
-}
